8a5d5cc8577b4c349454465d04aa9b664cd2707d
Christopher Alagna <christopheralagna1@gmail.com>
2026-09-13 00:50:48 -0700

    Cleaned WGUPS_Distances.csv and moved all task resources to a dedicated Resources folder

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from hash_table import HashTable
 from csv_reader import load_package_data
 
+#initialize hash table and load package data from CSV
 hash_table = HashTable()
 load_package_data("WGUPS_Packages.csv", hash_table)
 
+#search for and print all packages
 for i in range(1, 41):
     package = hash_table.search_package(i)
     print(package)
